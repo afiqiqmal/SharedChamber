f7fe07689d2f2525013f5c76bd8be5b4638c3812
Hafiq <hafiqiqmal93@gmail.com>
2017-03-24 16:53:39 +0800

    update map converter

--- library/src/main/java/com/zeroone/conceal/helper/ConverterListUtils.java
+++ library/src/main/java/com/zeroone/conceal/helper/ConverterListUtils.java
@@ -1,13 +1,19 @@
 package com.zeroone.conceal.helper;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author : hafiq on 23/03/2017.
  */
 
-public class ArrayUtils {
+public class ConverterListUtils {
 
     public static List<Integer> toIntArray(String string) {
         String[] strings = string.replace("[", "").replace("]", "").split(", ");
@@ -83,5 +89,42 @@ public class ArrayUtils {
 
     public static List<String> toStringArray(String string) {
         return Arrays.asList(string.replace("[", "").replace("]", "").split(", "));
+    }
+
+
+    public static String convertMapToString(Map<String,String> maps){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String key : maps.keySet()) {
+            if (stringBuilder.length() > 0) {
+                stringBuilder.append("&");
+            }
+            String value = maps.get(key);
+            try {
+                stringBuilder.append((key != null ? URLEncoder.encode(key, "UTF-8") : ""));
+                stringBuilder.append("=");
+                stringBuilder.append(value != null ? URLEncoder.encode(value, "UTF-8") : "");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException("This method requires UTF-8 encoding support", e);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static LinkedHashMap<String,String> convertStringToMap(String input){
+        LinkedHashMap<String, String> map = new LinkedHashMap<>();
+
+        String[] nameValuePairs = input.split("&");
+        for (String nameValuePair : nameValuePairs) {
+            String[] nameValue = nameValuePair.split("=");
+            try {
+                map.put(URLDecoder.decode(nameValue[0], "UTF-8"), nameValue.length > 1 ? URLDecoder.decode(nameValue[1], "UTF-8") : "");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException("This method requires UTF-8 encoding support", e);
+            }
+        }
+
+        return map;
     }
 }
